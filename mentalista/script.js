c31d9c3b13007b3c5d86c03d759e9c7126329863
ewerton1910 <ewertonjhonatas@hotmail.com>
2023-02-09 17:20:57 -0300

    :lipstick: feat: Adição de botão de reiniciar e melhorrias no código.

--- mentalista/script.js
+++ mentalista/script.js
@@ -1,31 +1,43 @@
-var numeroSecreto = parseInt(Math.random() * 10 + 1);
-console.log(numeroSecreto);
+var numeroSecreto = 0;
+var numeroSecreto = parseInt(Math.random() * 10);
+//console.log(numeroSecreto);
 var tentativas = 3;
+
+document.addEventListener(
+  "keypress",
+  function (e) {
+    if (e.which == 13) {
+      verificar();
+      document.querySelector("#chute").value = "";
+    }
+  },
+  false
+);
 
 function verificar() {
   while (tentativas > 0) {
-    
-   var input = document.querySelector("#chute");
-   var chute = parseInt(input.value)
+    var input = document.querySelector("#chute");
+    var chute = parseInt(input.value);
+    if (chute > 10 || chute < 1) {
+      h3.innerHTML = "Esse número não vale";
+      return;
+    }
 
     if (chute === numeroSecreto) {
-      
-      h3.innerHTML = "Você acertou o númwero é: "+ numeroSecreto;
+      h3.innerHTML = "você acertou";
       break;
-    } else if (chute > 10) {
-      h3.innerHTML = "Esse número não vale";
     } else if (chute > numeroSecreto) {
-      h3.innerHTML = "O número é menor";
+      h3.innerHTML = "Meu número é menor";
       tentativas = tentativas - 1;
       break;
     } else if (chute < numeroSecreto) {
-      h3.innerHTML = "O número é maior";
+      h3.innerHTML = "Meu número é maior";
       tentativas = tentativas - 1;
       break;
     }
   }
 
-  if (tentativas == 0) {
-    h3.innerHTML = "Você<br>perdeu!!O número era: " + numeroSecreto;
+  if (tentativas === 0) {
+    h3.innerHTML = "Você Perdeu!! <br> O número era: " + numeroSecreto + ".";
   }
 }
